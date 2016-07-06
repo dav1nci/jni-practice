20de2417935d66b6ceb6212aafb02fdb9e2ebd0c
Dima Stolpakov <dimskii123@yandex.ua>
2016-07-06 15:19:07 +0300

    going to linux

--- src/com/sock/test/ServerSocket.java
+++ src/com/sock/test/ServerSocket.java
@@ -7,6 +7,8 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Callable;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 /**
  * Created by stdima on 23.06.16.
@@ -27,7 +29,10 @@ public class ServerSocket implements Callable<String> {
             server.receive(buf);
             System.out.println("Server: message is \"" + new String(buf.getMessage()) + "\"");
             System.out.println("Message comes from " + buf.getHost() + " port: " + buf.getPort());
-            InetAddress client = InetAddress.getByAddress(BigInteger.valueOf(buf.getHost()).toByteArray());
+			byte[] hostBytes = ByteBuffer.wrap(BigInteger.valueOf(buf.getHost()).toByteArray())
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .array();
+            InetAddress client = InetAddress.getByAddress(hostBytes);
             server.send(new UDPPacket(buf.getMessage(),
                     buf.getBufLen(),
                     new InetSocketAddress(client, buf.getPort())));
